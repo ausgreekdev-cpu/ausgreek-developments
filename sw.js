7e757e70e98dae9378100568936b2e868ae8540c
Ausgreek Developments <hello@ausgreek.dev>
2026-09-23 21:47:49 +0800

    Use real favicon file, add privacy/terms/icon to SW cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
 const CACHE = 'ausgreek-v1';
-const ASSETS = ['/', '/index.html', '/css/style.css', '/js/main.js', '/manifest.json', '/404.html', '/thanks.html'];
+const ASSETS = ['/', '/index.html', '/css/style.css', '/js/main.js', '/manifest.json', '/404.html', '/thanks.html', '/privacy.html', '/terms.html', '/assets/icon.svg'];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
 self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
